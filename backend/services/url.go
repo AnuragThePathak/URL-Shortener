6637f16613577aa5db322d7b15527294fca94c08
Anurag Pathak <anuragpathak911@gmail.com>
2022-05-21 23:59:08 +0530

    implement service methods

--- backend/services/url.go
+++ backend/services/url.go
@@ -1,6 +1,10 @@
 package services
 
-import "context"
+import (
+	"context"
+
+	"github.com/osamingo/indigo"
+)
 
 type UrlStruct struct {
 	Url string `json:"url"`
@@ -12,4 +16,48 @@ type UrlService interface {
 	Get(context.Context, UrlStruct) (UrlStruct, error)
 
 	Delete(context.Context, UrlStruct) error
+}
+
+type urlService struct {
+	UrlStore UrlStore
+	IG       *indigo.Generator
+}
+
+func (u *urlService) Generate(
+	ctx context.Context, urlStruct UrlStruct) (UrlStruct, error) {
+	exists, err := u.UrlStore.CheckIfExists(ctx, urlStruct.Url)
+	if err != nil {
+		return UrlStruct{}, err
+	}
+	if exists {
+		return u.Get(ctx, urlStruct)
+	}
+
+	id, err := u.IG.NextID()
+	if err != nil {
+		return UrlStruct{}, err
+	}
+	if err := u.UrlStore.Create(ctx, id); err != nil {
+		return UrlStruct{}, err
+	}
+	return UrlStruct{Url: id}, nil
+}
+
+func (u *urlService) Get(
+	ctx context.Context, urlStruct UrlStruct) (UrlStruct, error) {
+	originalUrl, err := u.UrlStore.Get(ctx, urlStruct.Url)
+	if err != nil {
+		return UrlStruct{}, err
+	}
+	return UrlStruct{Url: originalUrl}, nil
+}
+
+type UrlStore interface {
+	CheckIfExists(context.Context, string) (bool, error)
+
+	Create(context.Context, string) error
+
+	Get(context.Context, string) (string, error)
+
+	Delete(context.Context) error
 }
