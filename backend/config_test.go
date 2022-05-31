16910d84afd0c3913630f5bc9185dcb5b8767712
Anurag Pathak <anuragpathak911@gmail.com>
2022-05-31 12:55:14 +0530

    add tests for server config

--- backend/config_test.go
+++ backend/config_test.go
@@ -5,19 +5,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/AnuragThePathak/url-shortener/backend/server"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func Test_databaseConnection(t *testing.T) {
 	tests := []struct {
-		setupDB    func()
+		setup      func()
 		assertions func(*mongo.Database, error)
 		name       string
 	}{
 		{
-			name:    "DB_URL not set",
-			setupDB: func() {},
+			name:  "DB_URL not set",
+			setup: func() {},
 			assertions: func(_ *mongo.Database, err error) {
 				require.Error(t, err)
 				require.Contains(t, err.Error(), "value not found for")
@@ -26,7 +27,7 @@ func Test_databaseConnection(t *testing.T) {
 		},
 		{
 			name: "DB_NAME not set",
-			setupDB: func() {
+			setup: func() {
 				os.Setenv("DB_URL", "mongodb://xyz")
 			},
 			assertions: func(_ *mongo.Database, err error) {
@@ -37,7 +38,7 @@ func Test_databaseConnection(t *testing.T) {
 		},
 		{
 			name: "success",
-			setupDB: func() {
+			setup: func() {
 				os.Setenv("DB_URL", "mongodb://xyz")
 				os.Setenv("DB_NAME", "new")
 			},
@@ -49,9 +50,102 @@ func Test_databaseConnection(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(_ *testing.T) {
-			test.setupDB()
+			test.setup()
 			db, err := databaseConnection(context.Background())
 			test.assertions(db, err)
+		})
+	}
+}
+
+func Test_serverConfig(t *testing.T) {
+	tests := []struct {
+		setup      func()
+		assertions func(server.ServerConfig, error)
+		name       string
+	}{
+		{
+			name: "PORT is not int",
+			setup: func() {
+				os.Setenv("PORT", "foo")
+			},
+			assertions: func(_ server.ServerConfig, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "was not parsable as an int")
+				require.Contains(t, err.Error(), "PORT")
+			},
+		},
+		{
+			name: "TLS_ENABLED is not bool",
+			setup: func() {
+				os.Setenv("PORT", "8080")
+				os.Setenv("TLS_ENABLED", "foo")
+			},
+			assertions: func(_ server.ServerConfig, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "was not parsable as a bool")
+				require.Contains(t, err.Error(), "TLS_ENABLED")
+			},
+		},
+		{
+			name: "TLS_ENABLED is false and success",
+			setup: func() {
+				os.Setenv("TLS_ENABLED", "false")
+			},
+			assertions: func(sc server.ServerConfig, err error) {
+				require.NoError(t, err)
+				require.Equal(t,
+					server.ServerConfig{
+						Port:       8080,
+						TLSEnabled: false,
+					},
+					sc)
+			},
+		},
+		{
+			name: "TLS_ENABLED is true and TLS_CERT_PATH not set",
+			setup: func() {
+				os.Setenv("TLS_ENABLED", "true")
+			},
+			assertions: func(_ server.ServerConfig, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "value not found for")
+				require.Contains(t, err.Error(), "TLS_CERT_PATH")
+			},
+		},
+		{
+			name: "TLS_ENABLED is true and TLS_KEY_PATH not set",
+			setup: func() {
+				os.Setenv("TLS_CERT_PATH", "/x/cert")
+			},
+			assertions: func(_ server.ServerConfig, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "value not found for")
+				require.Contains(t, err.Error(), "TLS_KEY_PATH")
+			},
+		},
+		{
+			name: "TLS_ENABLED is true and success",
+			setup: func() {
+				os.Setenv("TLS_KEY_PATH", "/x/key")
+			},
+			assertions: func(sc server.ServerConfig, err error) {
+				require.NoError(t, err)
+				require.Equal(t,
+					server.ServerConfig{
+						Port:        8080,
+						TLSEnabled:  true,
+						TLSCertPath: "/x/cert",
+						TLSKeyPath:  "/x/key",
+					}, sc)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(_ *testing.T) {
+			test.setup()
+			config, err := serverConfig()
+			test.assertions(config, err)
 		})
 	}
 }
