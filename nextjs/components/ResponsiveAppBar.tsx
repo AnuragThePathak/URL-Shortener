0192eebd433f6535d9e887d323d731f99597b2b6
Anurag Pathak <anuragpathak911@gmail.com>
2022-08-17 18:08:19 +0530

    fix issue of logo not redirecting to home

--- nextjs/components/ResponsiveAppBar.tsx
+++ nextjs/components/ResponsiveAppBar.tsx
@@ -35,7 +35,7 @@ const ResponsiveAppBar = () => {
 						variant="h5"
 						noWrap
 						component="a"
-						href=""
+						href="/"
 						sx={{
 							mr: 2,
 							display: { xs: 'flex', md: 'none' },
